4ac3c1fbd636642dfa800aec845e0cc2e41ed4ed
GioCri01 <giovanni.crispino94@icloud.com>
2022-05-20 11:56:10 +0200

    struttura esercizio

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+      <div class="container-fluid d-flex justify-content-between align-items-center">
+          <img src="https://www.romait.it/wp-content/uploads/2022/04/Netflix-logo.jpg" alt="">
+          <div class="d-flex ">
+              <input type="text" placeholder="cerca film" class="form-control mx-2">
+              <button class="btn bg-danger"> asd</button>
+          </div>
+      
+  </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: "HeaderComp",
+
+}
+</script>
+
+<style lang="scss" scoped>
+header{
+    height: 100px;
+    background-color: black;
+    img{
+        width: 175px;
+    }
+    
+}
+
+
+</style>
---
+++ src/components/MainComp.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+      m
+  </main>
+</template>
+
+<script>
+export default {
+    name: "MainComp",
+
+}
+</script>
+
+<style lang="scss" scoped>
+main{
+    width: 100%;
+    height: 100vh;
+    background-color: rgba(0, 0, 0, 0.514);
+}
+
+</style>
